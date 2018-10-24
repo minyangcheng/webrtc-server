e11a381b81eb1b8db19645faaaad115f3732c4ab
minyangcheng <332485508@qq.com>
2018-10-24 10:59:51 +0800

    视频连接完成

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const fs = require('fs');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const socketIo = require('socket.io');
+
+const app = express();
+var privateKey = fs.readFileSync(path.resolve(__dirname, '../certificate/private.pem'), 'utf8');
+var certificate = fs.readFileSync(path.resolve(__dirname, '../certificate/file.crt'), 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+const httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+const io = socketIo(httpServer);
+const ios = socketIo(httpsServer);
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
+httpServer.listen(8000, function () {
+  console.log('listening on *:8000');
+});
+httpsServer.listen(8001, function () {
+  console.log('listening on *:8001');
 });
 
 require('./socketHandler.js')(io);
+require('./socketHandler.js')(ios);
