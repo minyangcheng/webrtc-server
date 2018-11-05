0648967408c21c2fb01d2669c0a290f8f96ef943
minyangcheng <332485508@qq.com>
2018-11-05 16:25:16 +0800

    添加android客户端支持

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const http = require('http');
+// const http = require('http');
 const https = require('https');
 const socketIo = require('socket.io');
 
@@ -11,16 +11,16 @@ var certificate = fs.readFileSync(path.resolve(__dirname, '../certificate/file.c
 var credentials = {key: privateKey, cert: certificate};
 
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-const io = socketIo(httpServer);
+// const io = socketIo(httpServer);
 const ios = socketIo(httpsServer);
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-httpServer.listen(8000, function () {
-  console.log('listening on *:8000');
-});
+// httpServer.listen(8000, function () {
+//   console.log('listening on *:8000');
+// });
 httpsServer.listen(8001, function () {
   console.log('listening on *:8001');
 });
